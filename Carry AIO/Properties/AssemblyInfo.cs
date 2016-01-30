6c766e45357dbee1632be98c870b1dd55002ba0e
TopGunner <CeddyMoe@aol.com>
2016-01-30 09:35:00 +0100

    weird compile issues

--- Carry AIO/Properties/AssemblyInfo.cs
+++ Carry AIO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MissFortune")]
+[assembly: AssemblyTitle("CarryAIO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MissFortune")]
+[assembly: AssemblyProduct("CarryAIO")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
